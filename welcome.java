56b1301d7ed08ff786de191412eacdd169168ea2
dilip <syedjeelanbasha@outlook.com>
2018-09-13 07:13:54 +0000

    THis is the second commit

--- welcome.java
+++ welcome.java
@@ -3,6 +3,9 @@ public class HelloWorld {
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
         System.out.println("Hello, World");
+        System.out.println("AM Robot The chitti speed 1TB Memory 1ZB");
+       // System.out.println("Hello, World");
+        //System.out.println("Hello, World");
     }
 
 }
